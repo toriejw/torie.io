00bebf3374ef978ca0850e3671b03994c15dc17e
Torie Joy-Warren <victoria.joywarren@gmail.com>
2017-06-26 19:31:38 -0700

    Add post title, body, and date to show page
    - With simple styling and markdown rendering

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,8 @@
 class BlogPostsController < ApplicationController
 
   def show
+    @post = BlogPost.find(params[:id])
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true))
   end
 
 end
