d3c847f07dc15b06a714076152f54fae451ec785
Torie Joy-Warren <victoria.joywarren@gmail.com>
2017-10-02 19:45:58 -0700

    Adjust styling and fix code blocks

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,7 +2,7 @@ class BlogPostsController < ApplicationController
 
   def show
     @post = BlogPost.find(params[:id])
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true))
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true, fenced_code_blocks: true))
     PageView.track("Blog #{params[:id]}")
   end
 
