7268fa29401ecb92f5eaedf1089b4ab5ffec07c2
Torie Joy-Warren <victoria.joywarren@gmail.com>
2017-10-05 20:14:53 -0700

    Add posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,8 +29,88 @@ Classes have a few more keys in their hash. When you call a method on an object,
 If you'd like to go further in depth, I recommend [Vidar Hokstad's post](http://hokstad.com/ruby-object-model).
 }
 
+memoization_body =
+%Q{#What is memoization?
+
+Memoization is a technique used in programming to speed up accessor methods. For example, let’s say you have the following accessor method in the class `Person`:
+
+    class Person
+      def birth_name
+        @birth_name = birth_name.capitalize
+      end
+    end
+
+Every time we call the accessor method `birth_name` Ruby will perform the process of capitalizing the variable `birth_name`. But since a person’s birth name never changes, we shouldn’t need to re-format it every time.
+
+If we memoize this method, Ruby will no longer have to perform this process over and over. Instead, Ruby will cache the result, and next time we call the accessor method `birth_name` Ruby will already have the result stored in memory.
+
+#How do I use memoization?
+
+The simplest way to memoize your accessor methods is using this operator: `||=` . If we wanted to memoize the method from above, this is how we would do it:
+
+    class Person
+      def birth_name
+        @birth_name ||= birth_name.capitalize
+      end
+    end
+
+#How does it work?
+
+When you use the `||=` operator, what you’re essentially writing is this:
+
+    class Person
+      def birth_name
+        @birth_name ||= ( @birth_name || birth_name.capitalize )
+      end
+    end
+
+That’s saying that if `@birth_name` already exists, then return that. Otherwise, perform the operation `birth_name.capitalize`. If a result is already stored in memory, Ruby will simply return it. Otherwise it will create it.
+
+#When should I use memoization?
+
+In reality, we shouldn’t be using memoization for the method in the example above. The process of capitalizing a string isn’t costly enough to justify memoization. It should only be used for processes that are time-consuming, like loading in large amounts of data.
+
+**Memoization shouldn’t be used in place of good, well-optimized code.** It also should be avoided in multi-process applications, since a memoized variable can be stored as different things in different processes.
+}
+
+indexing_in_rails_body =
+%Q{#What is indexing?
+
+Database queries can be slow and make your application painful to use. One way to speed things up is to use indexing.
+
+Indexing is the process of building a data structure from a table column in order to search through that table more efficiently. The type of data structure that gets built depends on what type of database you’re using. Different databases have different options. In PostgreSQL there are 4 options: binary tree, hash, generalized search tree, or generalized inverted index. If you don’t specify a specific type, PostgreSQL defaults to binary tree.
+
+If a column on a table is indexed, and you search a table based on that column (eg. `Post.where(title: “My Post”)` – this searches the Post table based on the title column), the database query will traverse the data structure set up by indexing to find what it’s looking for.
+
+Using data structures like a binary tree to search increases the speed of that search immensely. Without it, every single row in a table would have to be looked at. With it, we can greatly reduce the number of table entries that need to be looked at. You can learn more about why binary tree’s are faster [here](http://www.stoimen.com/blog/2012/06/22/computer-algorithms-binary-search-tree-data-structure/).
+
+#How do I index a column?
+
+Assuming you have your database set up with at least one table, indexing a column isn’t too complicated.
+
+First, you need to create a rails migration:
+
+    $ rails g migration AddIndexToTable
+
+Then open the migration, and use Rails’s `add_index` method to create the index. The first parameter is the name of the table you want to add the index to. The second parameter is the name of the column you want to index:
+
+    def change
+      add_index :posts, :title
+    end
+
+Run the migration, and your database queries should be much faster!
+
+#Should I index all of my columns?
+
+No. While indexing speeds up reading from a database, it also slows down writing to a database. This is because every time it has to add something to a table, it now also has to update the data structure that was made for each indexed column. If your application does a lot more reading than writing, then this trade off is usually a good one, but you should only index columns that you actually query on. If you never need to search for a post based on its title, then it doesn’t make sense to index the title column of the Post table.
+
+Generally, join tables and foreign keys should be indexed. Primary ids are indexed automatically in Rails.
+}
+
 posts = [
-  { title: "The Ruby Object Model", body: object_model_body, created_at: Date.parse("20150912") }
+  { title: "The Ruby Object Model", body: object_model_body, created_at: Date.parse("20150912") },
+  { title: "Basic Memoization in Ruby", body: memoization_body, created_at: Date.parse("20151006") },
+  { title: "Performance Optimization and Indexing in Rails", body: indexing_in_rails_body, created_at: Date.parse("20160225") }
 ]
 
 posts.each do |post|
